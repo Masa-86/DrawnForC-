2e45727d29465fdecde4bd9151d0b145a5a4d855
Masa-86 <31477116+Masa-86@users.noreply.github.com>
2018-11-14 20:54:42 +0900

    Add files via upload

---
+++ TakeAPictura/TakeAPictura/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace TakeAPictura
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.StartUpDown = new System.Windows.Forms.Button();
+			this.StopUpDown = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(341, 49);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(109, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Take Off";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(341, 78);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(109, 23);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "Land";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(374, 165);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 2;
+			this.button3.Text = "Rec";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(375, 195);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(75, 23);
+			this.button4.TabIndex = 3;
+			this.button4.Text = "Stop";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// button5
+			// 
+			this.button5.Location = new System.Drawing.Point(12, 195);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(149, 23);
+			this.button5.TabIndex = 4;
+			this.button5.Text = "電池残量";
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.button5_Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(12, 165);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.ReadOnly = true;
+			this.textBox1.Size = new System.Drawing.Size(149, 22);
+			this.textBox1.TabIndex = 5;
+			// 
+			// StartUpDown
+			// 
+			this.StartUpDown.Location = new System.Drawing.Point(12, 49);
+			this.StartUpDown.Name = "StartUpDown";
+			this.StartUpDown.Size = new System.Drawing.Size(149, 23);
+			this.StartUpDown.TabIndex = 6;
+			this.StartUpDown.Text = "Start Up-Down";
+			this.StartUpDown.UseVisualStyleBackColor = true;
+			this.StartUpDown.Click += new System.EventHandler(this.StartUpDown_Click);
+			// 
+			// StopUpDown
+			// 
+			this.StopUpDown.Location = new System.Drawing.Point(13, 77);
+			this.StopUpDown.Name = "StopUpDown";
+			this.StopUpDown.Size = new System.Drawing.Size(148, 23);
+			this.StopUpDown.TabIndex = 7;
+			this.StopUpDown.Text = "Stop Up-Down";
+			this.StopUpDown.UseVisualStyleBackColor = true;
+			this.StopUpDown.Click += new System.EventHandler(this.StopUpDown_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(462, 246);
+			this.Controls.Add(this.StopUpDown);
+			this.Controls.Add(this.StartUpDown);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button StartUpDown;
+		private System.Windows.Forms.Button StopUpDown;
+	}
+}
+
---
+++ TakeAPictura/TakeAPictura/Form1.cs
@@ -0,0 +1,322 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TakeAPictura
+{
+	public partial class Form1 : Form
+	{
+		// TELLO接続文字列
+		private const string CONNECT_TELLO_IP = "192.168.10.1";
+		private const string RECIEVE_TELLO_IP = "0.0,0.0";
+		private const int CONNECT_TELLO_PORT = 8889;
+		private const int RECIEVE_VIDEO_PORT = 11111;
+
+		private UdpClient udpClient = null;
+
+		private bool isStopUpDown = false;
+		private bool canUp = true;
+
+		private enum RecieveMode
+		{
+			OKCancel = 0,
+			GetData,
+			GetVideo,
+			None
+		}
+
+		public Form1()
+		{
+			InitializeComponent();
+
+			var returnCommand = string.Empty;
+
+			// Tello1接続コマンドの送信
+			SendCommand("command");
+
+			while(!(returnCommand == "ok" || returnCommand == "error"))
+			{
+				returnCommand = UdpReciever(CONNECT_TELLO_PORT, CONNECT_TELLO_IP, (int)RecieveMode.OKCancel);
+			}
+		}
+
+		/// <summary>
+		/// コマンドの送信
+		/// </summary>
+		/// <param name="commandTxt"></param>
+		/// <returns></returns>
+		private int SendCommand(string commandTxt)
+		{
+			// UDPクライアントを生成する
+			if(udpClient == null)
+			{
+				udpClient = new UdpClient();
+			}
+
+			// TELLOへ送信するコマンドを生成する
+			string connectMessage = commandTxt;
+			byte[] byteMessage = Encoding.UTF8.GetBytes(connectMessage);
+
+			// TELLOへコマンドを送信する
+			int rcv = udpClient.Send(byteMessage, byteMessage.Length, CONNECT_TELLO_IP, CONNECT_TELLO_PORT);
+
+			return rcv;
+		}
+
+		/// <summary>
+		/// コマンドの受信
+		/// </summary>
+		/// <returns></returns>
+		private string UdpReciever(int recievePort,string recieveIP,int recieveMode)
+		{
+			var returnCommand = string.Empty;
+
+			// TimeOutを設定する
+			udpClient.Client.ReceiveTimeout = 300;
+			IPAddress telloAddress = null;
+
+			// IPとポートをバインドする
+			telloAddress = IPAddress.Parse(recieveIP);
+
+			// UDPのEndPointを生成する
+			var endPoint = new IPEndPoint(telloAddress, recievePort);
+
+			byte[] rcvByte = null;
+
+			try
+			{
+				// UDPを使用して、TELLOからデータを受信する
+				rcvByte = udpClient.Receive(ref endPoint);
+			}
+			catch (Exception ex)
+			{
+				return returnCommand;
+			}
+
+			if (rcvByte != null && rcvByte.Length > 0)
+			{
+				// 受信データを文字列に変換する
+				returnCommand = Encoding.UTF8.GetString(rcvByte);
+			}
+
+			return returnCommand;
+		}
+
+		/// <summary>
+		/// Take Off
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void button1_Click(object sender, EventArgs e)
+		{
+			var returnCommand = string.Empty;
+
+			SendCommand("takeoff");
+
+			while (!(returnCommand == "ok" || returnCommand == "error"))
+			{
+				returnCommand = UdpReciever(CONNECT_TELLO_PORT, CONNECT_TELLO_IP, (int)RecieveMode.OKCancel);
+			}
+		}
+
+		/// <summary>
+		/// Land
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void button2_Click(object sender, EventArgs e)
+		{
+			SendCommand("land");
+			UdpReciever(CONNECT_TELLO_PORT, CONNECT_TELLO_IP, (int)RecieveMode.OKCancel);
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void button3_Click(object sender, EventArgs e)
+		{
+			SendCommand("streamon");
+			UdpReciever(CONNECT_TELLO_PORT, CONNECT_TELLO_IP, (int)RecieveMode.OKCancel);
+			//UdpReciever(RECIEVE_VIDEO_PORT, RECIEVE_TELLO_IP, (int)RecieveMode.GetVideo);
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void button4_Click(object sender, EventArgs e)
+		{
+			SendCommand("streamoff");
+			UdpReciever(CONNECT_TELLO_PORT, CONNECT_TELLO_IP, (int)RecieveMode.OKCancel);
+		}
+
+		/// <summary>
+		/// バッテリー残量の確認
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void button5_Click(object sender, EventArgs e)
+		{
+			var isGetBattery = false;
+			int battery = -1;
+
+			SendCommand("battery?");
+
+			while(!isGetBattery)
+			{
+				var returnCommand = UdpReciever(CONNECT_TELLO_PORT, CONNECT_TELLO_IP, (int)RecieveMode.GetData);
+
+				if(int.TryParse(returnCommand,out battery))
+				{
+					isGetBattery = true;
+				}
+			}
+
+			this.textBox1.Text = battery.ToString();
+		}
+
+		/// <summary>
+		/// テローを一定の高さの範囲でUp-Downさせる
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void StartUpDown_Click(object sender, EventArgs e)
+		{
+			isStopUpDown = false;
+
+			this.StartUpDown.Enabled = false;
+
+			// タスクの実行
+			upDownTask = new Task(DoUpDown);
+			upDownTask.Start();
+
+		}
+
+		/// <summary>
+		/// Up-Downのストップ
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void StopUpDown_Click(object sender, EventArgs e)
+		{
+			isStopUpDown = true;
+
+			upDownTask.Wait();
+			upDownTask.Dispose();
+
+			this.StartUpDown.Enabled = true;
+		}
+
+		Task upDownTask;
+
+		/// <summary>
+		/// TELLOをUP/DOWNをさせる処理
+		/// </summary>
+		private void DoUpDown()
+		{
+			var returnCommand = string.Empty;
+
+			while (!isStopUpDown)
+			{
+				// 現在の高度を取得する
+				this.SendCommand("height?");
+
+				while(true)
+				{
+					returnCommand = this.UdpReciever(CONNECT_TELLO_PORT, CONNECT_TELLO_IP, (int)RecieveMode.None);
+
+					if(returnCommand.Contains("dm"))
+					{
+						break;
+					}
+				}
+
+				var height = CalcTelloHeight(returnCommand);
+
+				if(height == -1)
+				{
+					break;
+				}
+
+				Thread.Sleep(1000);
+
+				if (height >= 130)
+				{
+					// 高さが130cm以上の場合はdownコマンドで高度を下げる
+					this.SendCommand("down 20");
+					canUp = false;
+				}
+				else if(height <= 30)
+				{
+					// 高さが30cm以下の場合はupコマンドで高度を上げる
+					this.SendCommand("up 20");
+					canUp = true;
+				}
+				else
+				{
+					// 高さが30-130cmの間の場合は、前回と同じコマンドを実行する
+					if(canUp)
+					{
+						var up = 20;
+
+						if(height + 20 > 130)
+						{
+							up = 130 - height;
+						}
+
+						this.SendCommand("up " + up.ToString());
+					}
+					else
+					{
+						var down = 20;
+
+						if (height - 20 < 30)
+						{
+							down = height - 30;
+						}
+
+						this.SendCommand("down " + down.ToString());
+					}
+				}
+
+				returnCommand = this.UdpReciever(CONNECT_TELLO_PORT, CONNECT_TELLO_IP, (int)RecieveMode.None);
+
+				Thread.Sleep(1000);
+			}
+		}
+
+		/// <summary>
+		/// TELLOの現在の高さを計算する
+		/// </summary>
+		/// <param name="recieveMessage"></param>
+		/// <returns></returns>
+		private int CalcTelloHeight(string recieveMessage)
+		{
+			var height = 0;
+
+			// 高度は0dm\r\nの形式で受信されるため、dm\r\nの部分を除去する
+			// また、dm = 10cmのため、取得した高度を10倍して、cm単位で処理を行う
+			var heightString = recieveMessage.Replace("dm\r\n", "");
+
+			if (!int.TryParse(heightString, out height))
+			{
+				// 高さが取れなかった場合は異常値を返す
+				return -1;
+			}
+
+			return height * 10;
+		}
+	}
+}
